2cd3872dc69b689fcf8c4f48f3dfbe1584fcce62
K-Honsu <feranmia51@gmail.com>
2024-04-24 13:56:25 +0100

    feat: add throttler package and rate-limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,24 @@ import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
+import { ItemModule } from './item/item.module';
+import { OrderModule } from './order/order.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: ".env",
     isGlobal: true
-  }), MongooseModule.forRoot(process.env.DATABASE_URL), UserModule, AuthModule, CategoryModule],
+  }), MongooseModule.forRoot(process.env.DATABASE_URL), UserModule, AuthModule, CategoryModule, ItemModule, OrderModule, ThrottlerModule.forRoot([{
+    ttl: 60000,
+    limit: 10,
+  }])],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard
+  }],
 })
 export class AppModule { }
